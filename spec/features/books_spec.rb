b35abff7063df4646fd920a19902aa694b7c3a2b
rahul <rahulsingh2240@gmail.com>
2017-10-24 23:00:23 +0530

    coverage

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Books", type: :feature do
   context "create new user" do
-  	scenario "should be successfill" do 
+  	scenario "should be successfull" do 
   		visit root_path
   			click_button 'New Book'
   			within('form[id="new_book"]') do
@@ -12,6 +12,22 @@ RSpec.feature "Books", type: :feature do
   				fill_in 'Isbn', with: 22
   			end
   			click_button 'Save'
+        expect(page).to have_content('Book created')
   	end
+
+    scenario "should fail" do 
+      visit root_path
+        click_button 'New Book'
+        within('form[id="new_book"]') do
+          fill_in 'Name', with: ''
+          fill_in 'Category', with: 'web'
+          fill_in 'Price', with: 20
+          fill_in 'Isbn', with: 22
+        end
+        click_button 'Save'
+    end
+
   end
+
+
 end
